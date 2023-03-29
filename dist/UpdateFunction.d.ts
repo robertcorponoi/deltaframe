b90b6a092a823691a23917ed28ebea7ad31e7b68
Robert Corponoi <robertcorponoi@gmail.com>
2023-03-29 18:49:15 -0400

    Removed dist directory and added to .gitignore

--- dist/UpdateFunction.d.ts
+++
@@ -1,7 +0,0 @@
-/** The structure of the function that is used by the RAF loop. */
-export type RAFUpdateFunction = (timestamp: number) => void;
-/**
- * The structure of the function that should be called every time the loop
- * updates.
- */
-export type UpdateFunction = (delta: number, deltaAverage: number, timestamp: number) => void;
